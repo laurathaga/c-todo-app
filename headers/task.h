36c0436d1f4d3a1e35de59e0a2057025d674fd4c
Laurat <l4tt1haga@gmail.com>
2024-07-14 10:36:20 +0200

    adding types

---
+++ headers/task.h
@@ -0,0 +1,20 @@
+typedef enum _status {
+  UNDONE,
+  DONE,
+} Status;
+
+struct _task {
+  unsigned long id;
+  char *title;
+  Status status;
+};
+
+typedef struct _task Task;
+
+void init_tasks(void);
+
+void create_task(char *title, Status status);
+
+void delete_task(void);
+
+Task *find_task(char *title);
