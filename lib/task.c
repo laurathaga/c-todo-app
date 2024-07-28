a5a7ca150f4875d61c668e8a960fb433e7e7e31e
Laurat <l4tt1haga@gmail.com>
2024-07-28 13:55:19 +0200

    reading into buffer

--- lib/task.c
+++ lib/task.c
@@ -3,31 +3,51 @@
 
 #include "../headers/task.h"
 #include "../headers/common.h"
+#include "../headers/db.h"
 
 #define BUFFER_SIZE 10
 
 size_t id_index = 1;
 size_t buffer_size = 0;
 size_t length = 0;
-static Task **tasks = NULL;
+static Task **tasks_buffer = NULL;
 static Task **tasks_ptr = NULL;
 
 void init_tasks(void)
 {
-  if (tasks != NULL)
+  if (tasks_buffer != NULL)
     return;
 
   buffer_size = BUFFER_SIZE * sizeof(Task *);
 
-  tasks = (Task **) calloc(0, buffer_size);
+  tasks_buffer = (Task **) calloc(0, buffer_size);
 
-  if (tasks == NULL)
+  if (tasks_buffer == NULL)
   {
     printf("could not allocate memory for task list\n");
     exit(EXIT_FAILURE);
   }
 
-  tasks_ptr = tasks;
+  tasks_ptr = tasks_buffer;
+
+  FILE *file = fopen(DB_NAME, "rb");
+
+  if (file == NULL) 
+  {
+    printf("Could not open file %s, please try again\n", DB_NAME);
+    exit(EXIT_FAILURE);
+  }
+
+  while (!feof(file))
+  {
+    fread(*tasks_ptr, sizeof(Task), 1, file);
+
+    printf("title of task is %s\n", (*tasks_ptr)->title);
+
+    *tasks_ptr++;
+  }
+
+  printf("read_result is %d\n", read_result);
 }
 
 void create_task(void)
