673562c3b2e3db60853e63862be71d4d2adcbdd5
Laurat <l4tt1haga@gmail.com>
2024-08-15 10:49:05 +0200

    fixing intro

--- main.c
+++ main.c
@@ -6,9 +6,17 @@ int main(void)
   char op = '\0';
   init_tasks();
 
-  printf("====== TODO APP ======\n");
+  printf("\n====== TODO APP ======\n\n");
 
-  printf("Please enter operation: ");
+  printf("In order to interact with the app you have to press: \n\n");
+  printf("i - for inserting new todo; \n");
+  printf("u - for updating an existing todo; \n");
+  printf("r - for reading all the todos; \n");
+  printf("d - for deleting a todo. \n");
+
+  printf("\n======================\n\n");
+
+  printf("Please enter command: ");
   scanf(" %c", &op);
 
   handle_op(&op);
